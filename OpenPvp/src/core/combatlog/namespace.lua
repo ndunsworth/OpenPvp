8d56ec7ba23726e215b3b3e10ca996245af9273d
Nathan Dunsworth <nathandunsworth@gmail.com>
2025-06-21 03:28:24 -0500

    Misc refactor

--- OpenPvp/src/core/combatlog/namespace.lua
+++ OpenPvp/src/core/combatlog/namespace.lua
@@ -25,8 +25,8 @@
 -- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 -- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-local _, OpenPvpLib = ...
-local opvp = OpenPvpLib;
+local _, OpenPvp = ...
+local opvp = OpenPvp;
 
 opvp.combatlog = {
     ARENA_MATCH_START          = "ARENA_MATCH_START",
